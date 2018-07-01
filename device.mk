9252e50f5b401289744c72b3e5a2003c4ed961e3
LuK1337 <priv.luk@gmail.com>
2018-07-01 14:41:43 +0200

    cedric: Remove unnecessary languages_full.mk include
    * It's already included in full_base_telephony.
    
    Change-Id: I7547ff447e81641e74fd911a12f5a9f0df9e5ccf

--- device.mk
+++ device.mk
@@ -14,8 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
