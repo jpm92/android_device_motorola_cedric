9c79d8874033087bbcb5d43c7995d175ae0055e3
JarlPenguin <jarlpenguin@outlook.com>
2019-06-14 18:44:19 +0300

    sepolicy: build it
    Signed-off-by: JarlPenguin <jarlpenguin@outlook.com>
    Change-Id: Ia0596fabe1aae98da87623c5deb1d2e293824ad9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -219,7 +219,7 @@ TARGET_RIL_VARIANT := caf
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Sensor
 BOARD_USES_MOT_SENSOR_HUB := true
