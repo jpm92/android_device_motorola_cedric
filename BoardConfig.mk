schanne/android_device_motorola_montana

montana: split vendor specific and system props
Change-Id: I642a7a0f1d43b8cd94b3bc2def476d60a4d66f39

BoardConfig.mk
@@ -236,6 +236,8 @@ TARGET_LD_SHIM_LIBS := \
 TARGET_COPY_OUT_VENDOR := vendor
 BOARD_VENDORIMAGE_PARTITION_SIZE := 665845760
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
+TARGET_VENDOR_PROP += $(PLATFORM_PATH)/vendor.prop
 
 # Thermal
 USE_DEVICE_SPECIFIC_THERMAL := true

jpm92/android_device_motorola_cedric

BoardConfig.mk
@@ -273,3 +273,5 @@ PRODUCT_FULL_TREBLE_OVERRIDE := true
 TARGET_COPY_OUT_VENDOR := vendor
 BOARD_VENDORIMAGE_PARTITION_SIZE := 687865856
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop