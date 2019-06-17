41088c464a98e293ae3e61418a691dbbf4337e14
Jarl-Penguin <jarlpenguin@outlook.com>
2019-06-18 01:30:26 +0300

    cedric: re-organize seccomp policy
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- device.mk
+++ device.mk
@@ -356,8 +356,8 @@ PRODUCT_COPY_FILES += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp_policy/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
 # IMS
 PRODUCT_PACKAGES += \
